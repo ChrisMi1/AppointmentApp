e62226b908268a14f3a750af01124b039692fc31
ChrisMi1 <xristosmalt@hotmail.com>
2024-04-16 11:51:27 +0300

    Mapping the classes with the entities.

--- src/main/java/com/iceuniwastudents/AppointmentApp/AppointmentAppApplication.java
+++ src/main/java/com/iceuniwastudents/AppointmentApp/AppointmentAppApplication.java
@@ -1,13 +1,7 @@
 package com.iceuniwastudents.AppointmentApp;
 
-import com.iceuniwastudents.AppointmentApp.Repository.InventoryRepo;
-import com.iceuniwastudents.AppointmentApp.model.User;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class AppointmentAppApplication {
